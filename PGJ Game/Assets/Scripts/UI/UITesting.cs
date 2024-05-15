f6c91489c69a6c2fe7b20aea261da8a15c2c2a50
ghrimbone <ghrimbone@gmail.com>
2024-05-15 13:25:09 +1000

    End screen works!!
    Fixed end screen, changed how scoring works, animation added

--- PGJ Game/Assets/Scripts/UI/UITesting.cs
+++ PGJ Game/Assets/Scripts/UI/UITesting.cs
@@ -1,27 +1,22 @@
 using UnityEngine;
 
-public class UITesting : MonoBehaviour
-{
-    public EndScreen endScreen;
-    public PlayerData data;
+public class UITesting : MonoBehaviour {
+	public EndScreen endScreen;
+	public PlayerData data;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        InvokeRepeating("SwapVic", 4f, 4f);
-        InvokeRepeating("SwapDef", 2f, 4f);
-    }
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start() {
+		InvokeRepeating("SwapVic", 4f, 4f);
+		InvokeRepeating("SwapDef", 2f, 4f);
+	}
 
-    void SwapVic()
-    {
-        endScreen.Victory();
-        data.MoveCount += 2;
-    }
+	void SwapVic() {
+		endScreen.Victory();
+		data.MoveCount += 2;
+	}
 
-    void SwapDef()
-    {
-        endScreen.Defeat();
-        data.MoveCount -= 1;
-    }
-
+	void SwapDef() {
+		endScreen.Defeat();
+		data.MoveCount -= 1;
+	}
 }
