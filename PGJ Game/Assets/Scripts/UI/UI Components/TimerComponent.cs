acf06d48664ec3e00523db9da206a03bb10df3a0
WarlordSdocy <docystephen@gmail.com>
2024-05-19 17:17:28 -0700

    Changed out UI sprites
    Swapped out some UI sprites to be more pixely.

--- PGJ Game/Assets/Scripts/UI/UI Components/TimerComponent.cs
+++ PGJ Game/Assets/Scripts/UI/UI Components/TimerComponent.cs
@@ -44,12 +44,12 @@ public partial class TimerComponent : VisualElement
         float height = contentRect.height;
 
         var painter = context.painter2D;
-        painter.BeginPath();
-        painter.lineWidth = 10f;
         float size = Mathf.Min(width, height);
+        /*painter.BeginPath();
+        painter.lineWidth = 10f;
         painter.Arc(new Vector2(width * 0.5f, height * 0.5f), size * 0.5f, 0f, 360f);
         painter.ClosePath();
-        painter.Stroke();
+        painter.Stroke();*/
 
         painter.BeginPath();
         painter.lineWidth = 7.5f;
